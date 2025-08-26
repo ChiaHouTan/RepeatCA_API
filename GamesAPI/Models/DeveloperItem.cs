cfe8d54b8e18b8607170b223c0d9f3df541f4c49
ChiaHouTan <57621046+ChiaHouTan@users.noreply.github.com>
2025-08-26 21:45:40 +0100

    create project and set up table items

---
+++ GamesAPI/Models/DeveloperItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesAPI.Models
+{
+    public class DeveloperItem
+    {
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        public string? DeveloperName { get; set; }
+        public DateTime FoundedYear { get; set; }
+        public string? Country { get; set; }
+        public bool IsIndependent { get; set; }
+        public ICollection<GameItem> Games { get; set; } = new List<GameItem>();
+    }
+}
---
+++ GamesAPI/Models/GameItem.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GamesAPI.Models
+{
+    public class GameItem
+    {
+        public enum genre
+        {
+            Action,
+            Adventure,
+            RPG,
+            Puzzle,
+            Strategy,
+            Simulation,
+            Sports,
+            Horror,
+            Shooter,
+            Racing,
+            Other
+        }
+        [Key]
+        public Guid ID { get; set; }
+        public string? Title { get; set; }
+        public string? Image { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public genre Genre { get; set; }
+        public double Price { get; set; }
+        public double Rating { get; set; }
+        [ForeignKey("AlbumID")]
+        public DeveloperItem? Developer { get; set; }
+        public Guid DeveloperID { get; set; }
+    }
+}
